fb0a1d20b87f14f49fc9862b421dcf743b63032f
fanqh <qinghua.fan@redflower.com.cn>
2018-03-09 08:55:31 +0800

    the first commit

---
+++ app/src/main/java/com/example/rxandroid_master/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.rxandroid_master;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+
+//        /*throttleFirst防止重复点击*/
+//        Observable<Object> clicks = RxView.clicks(tv).throttleFirst(1000, TimeUnit.MILLISECONDS);
+//        clicks.subscribe(new Consumer<Object>() {
+//            @Override
+//            public void accept(Object o) throws Exception {
+//                /*响应单击click事件*/
+//                Toast.makeText(MainActivity.this, "别点我！", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        Observable<List<Object>> listObservable = RxView.clicks(tv)
+//                .buffer(1000, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread(), 2);
+//        listObservable.subscribe(new Consumer<List<Object>>() {
+//            @Override
+//            public void accept(List<Object> objects) throws Exception {
+//                if (objects.size() == 2) {
+//                    /*响应双击click事件*/
+//                    Toast.makeText(MainActivity.this, "双击我！", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+
+        RxView.clicks(tv).throttleFirst(1000, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                        startActivityForResult(intent, 0);
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("TAG", "onActivityResult() is to be call");
+    }
+}
---
+++ app/src/main/java/com/example/rxandroid_master/MessageActivity.java
@@ -0,0 +1,17 @@
+package com.example.rxandroid_master;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fanqh on 2018/3/2.
+ */
+
+public class MessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
